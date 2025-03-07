72da03aec5317b7f7cb4428970815020f35c3bdd
isak148 <isak.heggland36@gmail.com>
2025-03-07 15:49:07 +0100

    therding2

--- samling/Samlet_kode.py
+++ samling/Samlet_kode.py
@@ -1,34 +1,35 @@
 import subprocess
 import threading
-import time 
+import time
 
 def funksjon1():
+    print("Funksjon 1 startet")
     subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/VL6180X/examples/vl6180x_continuoustest.py"])
-    print("er ferdig")
+    print("Funksjon 1 er ferdig")
     time.sleep(0.1)
 
 def funksjon2():
-    subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/mpu6050_1/mpu6050_2/Bachelor_test1.py"])  
-    print("er ferdig")
+    print("Funksjon 2 startet")
+    subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/mpu6050_1/mpu6050_2/Bachelor_test1.py"])
+    print("Funksjon 2 er ferdig")
     time.sleep(1)
 
 def funksjon3():
+    print("Funksjon 3 startet")
     subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/ms5837/example.py"])
-    
-    print("er ferdig")
+    print("Funksjon 3 er ferdig")
     time.sleep(1)
 
 tred1 = threading.Thread(target=funksjon1)
 tred2 = threading.Thread(target=funksjon2)
 tred3 = threading.Thread(target=funksjon3)
 
-tred1.start
-tred3.start
-tred2.start
+tred1.start()
+tred3.start()
+tred2.start()
 
-tred1.join
-tred2.join
-tred3.join 
+tred1.join()
+tred2.join()
+tred3.join()
 
-print("hovud tråd ferdig")
-
+print("Hovedtråd ferdig")
