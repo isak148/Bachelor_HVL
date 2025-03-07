b5dcb6013dd209d38291084a69ef6f3945744b50
Isak Heggland <isak.heggland36@gmail.com>
2025-03-07 15:16:43 +0000

    datapunkt

--- samling/Samlet_kode.py
+++ samling/Samlet_kode.py
@@ -22,6 +22,7 @@ def funksjon3():
     print("Funksjon 3 er ferdig")
     time.sleep(1)
 
+
 tred1 = threading.Thread(target=funksjon1)
 tred2 = threading.Thread(target=funksjon2)
 tred3 = threading.Thread(target=funksjon3)
