701382bfc788818972e95b068561b16c7fc536e3
Isak Heggland <isak.heggland36@gmail.com>
2025-03-07 15:00:42 +0000

    commit: threding

--- samling/Samlet_kode.py
+++ samling/Samlet_kode.py
@@ -2,21 +2,23 @@ import subprocess
 import threading
 import time
 
+def ny_terminal(script_path):
+    subprocess.Popen(["lxterminal","-e","python3",script_path])
 def funksjon1():
     print("Funksjon 1 startet")
-    subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/VL6180X/examples/vl6180x_continuoustest.py"])
+    ny_terminal("/home/Bachelor/Bachelor_HVL/VL6180X/examples/vl6180x_continuoustest.py")
     print("Funksjon 1 er ferdig")
     time.sleep(0.1)
 
 def funksjon2():
     print("Funksjon 2 startet")
-    subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/mpu6050_1/mpu6050_2/Bachelor_test1.py"])
+    ny_terminal("/home/Bachelor/Bachelor_HVL/mpu6050_1/mpu6050_2/Bachelor_test1.py")
     print("Funksjon 2 er ferdig")
     time.sleep(1)
 
 def funksjon3():
     print("Funksjon 3 startet")
-    subprocess.run(["python3", "/home/Bachelor/Bachelor_HVL/ms5837/example.py"])
+    ny_terminal("/home/Bachelor/Bachelor_HVL/ms5837/example.py")
     print("Funksjon 3 er ferdig")
     time.sleep(1)
 
