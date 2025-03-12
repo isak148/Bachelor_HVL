c58759aba62409fd12fd684d763f36d2d6a2a344
isak148 <isak.heggland36@gmail.com>
2025-03-12 11:26:59 +0100

    ad up

--- ArduinoAD8232ECG-master/ads1015.py
+++ ArduinoAD8232ECG-master/ads1015.py
@@ -1,32 +1,62 @@
 import time
-import board
-import busio
-import matplotlib.pylab as plt
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+import matplotlib.pyplot as plt
+import Adafruit_ADS1x15
+import numpy as np
 
+# ADS1015 konfigurasjon
+ADC = Adafruit_ADS1x15.ADS1015()
+GAIN = 1  # Juster om nødvendig
+CHANNEL = 0  # Koble AD8232 til kanal 0
 
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+# Variabler for plotting og pulsmåling
+sample_rate = 250  # Prøver per sekund (juster om nødvendig)
+buffer_size = 2500  # 10 sekunder med data
+data_buffer = np.zeros(buffer_size)
+time_buffer = np.zeros(buffer_size)
+pulse_history = []
 
-# Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
+def get_adc_value():
+    return ADC.read_adc(CHANNEL, gain=GAIN)
 
-# Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
-x = chan.value
-y = chan.voltage
+def calculate_pulse(data, sample_rate):
+    # Enkel pulsmåling: Teller topper i signalet
+    threshold = (np.max(data) - np.min(data)) * 0.6 # Finne toppene i signalet.
+    peaks = np.where((data[1:-1] > threshold) & (data[1:-1] > data[0:-2]) & (data[1:-1] > data[2:]))[0] + 1
+    if len(peaks) > 1:
+        time_diffs = np.diff(peaks) / sample_rate
+        if len(time_diffs) > 0 :
+            mean_time_diff = np.mean(time_diffs)
+            if mean_time_diff > 0:
+                pulse = 60 / mean_time_diff
+                pulse_history.append(pulse)
+                if len(pulse_history) > 10: # Tar et gjennomsnitt av de 10 siste pulsene.
+                  pulse_history.pop(0)
+                return np.mean(pulse_history)
+            else:
+                return None
 
-# Create differential input between channel 0 and 1
-#chan = AnalogIn(ads, ADS.P0, ADS.P1)
-if y <= 2.5:
-    last_time = current_time - last_time
+def update_plot():
+    plt.clf()
+    plt.plot(time_buffer, data_buffer)
+    plt.xlabel('Tid (sekunder)')
+    plt.ylabel('ADC Verdi')
+    plt.title('EKG Signal')
+    if pulse is not None:
+      plt.text(0.05, 0.95, f'Estimert Puls: {pulse:.2f} BPM', transform=plt.gca().transAxes)
+    plt.pause(0.01)
 
+# Hovedløkke
+try:
+    start_time = time.time()
+    for i in range(buffer_size):
+        data_buffer[i] = get_adc_value()
+        time_buffer[i] = time.time() - start_time
+        time.sleep(1 / sample_rate)
+        if i > sample_rate: #Måles ikke puls før det har gått et sekund.
+          pulse = calculate_pulse(data_buffer[0:i], sample_rate)
 
-
-print("{:>5}\t{:>5}".format('raw', 'v'))
-
-while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-
-    time.sleep(0.5)
+        if i % (sample_rate / 2) == 0:  # Oppdater plottet hvert halve sekund
+            update_plot()
+except KeyboardInterrupt:
+    print('Avslutter...')
+    plt.close()
